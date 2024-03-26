b4494d52512c26ff656575b7f7cdceb3e13eb4be
Prabha-2002 <163090094+Prabha-2002@users.noreply.github.com>
2024-03-26 09:17:09 +0530

    Javascript of day1 and day2 are commited

---
+++ Day 1/sample.js
@@ -0,0 +1,2 @@
+  document.write(" <h1> <font color= blue>Hello World </h1>");
+             // console.log("Hello World"); 
---
+++ Day 2/stopwatch.js
@@ -0,0 +1,3 @@
+let startbtn = document.getElementById("start");
+let stopbtn = document.getElementById("stop");
+let resetbtn = document.getElementById("reset");
