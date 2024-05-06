60fa4eae9b64fb3daa49f044ebbb6412168eced8
Hardin-J <105577218+Hardin-J@users.noreply.github.com>
2024-05-06 09:55:56 +0530

    Add files via upload

---
+++ 12207-Jeevarajan-R/L1/React/Day-1/src/Comp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Comp() {
+  return (
+    <div>
+      <h1>This is newComp component</h1>
+    </div>
+  )
+}
+
+export default Comp;
---
+++ 12207-Jeevarajan-R/L1/React/Day-1/src/New.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function New() {
+  return (
+    <div>
+      <h1>This is New Component</h1>
+    </div>
+  )
+}
+
+export default New
