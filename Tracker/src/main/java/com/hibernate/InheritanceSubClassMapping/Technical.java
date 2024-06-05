e4df5a072358e8ff92784334cd8d51b7cdb6ae6c
Vashanth TS <115968014+Vip-Vashi@users.noreply.github.com>
2024-06-05 09:50:59 +0530

    HibernateTask-2

---
+++ Tracker/src/main/java/com/hibernate/InheritanceSubClassMapping/Technical.java
@@ -0,0 +1,33 @@
+package com.hibernate.InheritanceSubClassMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TechnicalJoins")
+public class Technical extends Tracker{
+
+		int marks;
+
+		public int getMarks() {
+			return marks;
+		}
+
+		public void setMarks(int marks) {
+			this.marks = marks;
+		}
+
+		public Technical() {
+			super();
+			
+		}
+
+		public Technical(int empid, String empname, String role, int marks) {
+			super(empid, empname, role);
+			this.marks = marks;
+		}
+
+		
+		
+		
+}
---
+++ Tracker/src/main/java/com/hibernate/InheritanceTableClassMapping/App.java
@@ -0,0 +1,37 @@
+package com.hibernate.InheritanceTableClassMapping;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.jdbc.BatchedTooManyRowsAffectedException;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	Configuration cfg = new Configuration().configure("tableperclass.cfg.xml");
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session =sf.openSession();
+        Transaction tx = session.beginTransaction();
+    	Tracker tr = new Tracker(1,"Gokul","Trainee");
+        Technical te = new Technical(2,"raj","Trainee",80);
+        Behavioral be = new Behavioral(75,"GOkul","Trainee",90,"Rajesh","TrustWorthy");
+        Java j = new Java(101,"Subash","Trainee",95,"Spring","Sena");
+        Dotnet d = new Dotnet(100,"Raju","Trainee",90,"Collections","Anish");
+        session.save(tr);
+        session.save(te);
+        session.save(be);
+        session.save(j);
+        session.save(d);
+        
+        tx.commit();
+
+    }
+}
