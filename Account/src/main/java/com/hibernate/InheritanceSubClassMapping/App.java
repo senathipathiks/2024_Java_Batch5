3bb1514b356c6c1727a0db2ff52be510a4c7cc90
Vashanth TS <115968014+Vip-Vashi@users.noreply.github.com>
2024-06-05 09:52:33 +0530

    HibernateTask-3

---
+++ Account/src/main/java/com/hibernate/InheritanceSubClassMapping/App.java
@@ -0,0 +1,35 @@
+package com.hibernate.InheritanceSubClassMapping;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.jdbc.BatchedTooManyRowsAffectedException;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	Configuration cfg = new Configuration().configure("subclass.cfg.xml");
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session =sf.openSession();
+        Transaction tx = session.beginTransaction();
+        Account acc = new Account(12228,6379,"Nagarjun N S","Indian Overseas Bank","Teppakulam");
+        Savings save = new Savings(12229,9894,"Vashanth TS","Indian Bank","Palanganatham","Savings",4,500);
+        Current curr = new Current(12230,7873,"Nandha","Punjab Bank","Ellis Nagar","Current",0,5000);
+        
+        session.save(acc);
+        session.save(save);
+        session.save(curr);
+        
+        tx.commit();
+
+
+    }
+}
