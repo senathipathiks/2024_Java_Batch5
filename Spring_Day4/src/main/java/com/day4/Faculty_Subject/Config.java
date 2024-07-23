42dc9ac54e2331a783941b4279149b92282627e8
senathipathiks <88962767+senathipathiks@users.noreply.github.com>
2024-07-23 12:06:48 +0530

    Delete Spring_Day4 directory

--- Spring_Day4/src/main/java/com/day4/Faculty_Subject/Config.java
+++
@@ -1,9 +0,0 @@
-package com.day4.Faculty_Subject;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.day4.Faculty_Subject")
-public class Config {
-}
--- Spring_Day4/src/main/java/com/day4/Laptop/Config.java
+++
@@ -1,9 +0,0 @@
-package com.day4.Laptop;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.day4.Laptop_Specs")
-public class Config {
-}
