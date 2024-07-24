82c5078399f2b0df12b4eefbc5540d8bfb8de220
Nandhakumaran6403 <123458132+Nandhakumaran6403@users.noreply.github.com>
2024-07-24 16:35:33 +0530

    Add files via upload

---
+++ FullStack-Practices/Restaurant/Restaurant/myapp/src/Components/Admin/Section.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Section() {
+  return (
+    <div style={{ 
+      backgroundImage: `url('https://wallpapers.com/images/hd/food-4k-m37wpodzrcbv5gvw.jpg')`,
+      backgroundSize: 'cover',
+      height: '900px',
+      position: 'relative', // Necessary for absolute positioning of video
+    }}>
+    </div>
+  );
+}
+
+export default Section;
+
