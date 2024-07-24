402df6986267bba43194eab2b55af77580fedab1
Nandhakumaran6403 <123458132+Nandhakumaran6403@users.noreply.github.com>
2024-07-24 16:51:04 +0530

    Add files via upload

---
+++ Testing/sonarcheck/src/main/java/com/nandha/sonarcheck/App.java
@@ -0,0 +1,13 @@
+package com.nandha.sonarcheck;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
