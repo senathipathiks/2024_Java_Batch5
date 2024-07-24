09ead08f3ac9d1deaff3647ea35cb32153cde2d7
Nandhakumaran6403 <123458132+Nandhakumaran6403@users.noreply.github.com>
2024-07-24 15:47:45 +0530

    Add files via upload

---
+++ Frontend-Practices/React/ReactRedux1/src/App.js
@@ -0,0 +1,52 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import './App.css';
+import { Provider } from 'react-redux';
+import store from './store.js';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/cart" component={Cart} />
+        </Switch>
+        </BrowserRouter>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
