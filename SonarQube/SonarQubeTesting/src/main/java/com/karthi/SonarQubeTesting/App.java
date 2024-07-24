a3f86aabf6d0d2f506080397779edb6c245fc1ee
RKKarthikeyan2002 <95752663+RKKarthikeyan2002@users.noreply.github.com>
2024-07-24 11:26:58 +0530

    Add files via upload
    SonarQube Uploaded

---
+++ SonarQube/SonarQubeTesting/src/main/java/com/karthi/SonarQubeTesting/App.java
@@ -0,0 +1,13 @@
+package com.karthi.SonarQubeTesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
