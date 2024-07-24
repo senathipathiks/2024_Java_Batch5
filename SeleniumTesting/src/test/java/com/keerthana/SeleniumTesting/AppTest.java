0730ac6efdf183e47976bdca3319202663790c0c
Keerthanaprabu <146918327+keerthana1607@users.noreply.github.com>
2024-07-24 09:44:50 +0530

    Add files via upload

---
+++ SeleniumTesting/src/test/java/com/keerthana/SeleniumTesting/AppTest.java
@@ -0,0 +1,13 @@
+package com.keerthana.SeleniumTesting;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+   
+}
